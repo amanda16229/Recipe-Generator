932f5df2af67b863a810490733fd446344a5966e
amanda16229 <amanda.olyer@gmail.com>
2024-10-02 12:30:22 -0400

    learning GUIS w Kivy

--- AppDemo.py
+++ AppDemo.py
@@ -35,37 +35,4 @@ class HelloKivyApp(MDApp):
 
 
 
-#HelloKivyApp().run()
-
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatter import Scatter
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-
-class Kivy(App):
-    def build(self):
-        b = BoxLayout(orientation='vertical')
-
-        t = TextInput(font_size = 50, size_hint_y = None, height = 100)
-        b.add_widget(t)
-
-        f = FloatLayout()
-        b.add_widget(f)
-        s = Scatter()
-        b.add_widget(s)
-        # l = Label(text = "Hello!", font_size = 50)
-        # f.add_widget(s)
-        # s.add_widget(l)
-        #
-        # b.add_widget(t)
-        # b.add_widget(f)
-        #
-        # # Binding it with the label
-        # t.bind(text=l.setter('text'))
-
-        return b
-
-Kivy().run()
+HelloKivyApp().run()
---
+++ Checkbox Slider.py
@@ -0,0 +1,49 @@
+# import Checkbox to allow features of slider
+from kivy.uix.checkbox import CheckBox
+
+# Program to learn how to make checkbox in kivy
+
+# import kivy module
+import kivy
+
+# base Class of your App inherits from the App class.
+# app:always refers to the instance of your application
+from kivy.app import App
+
+# The :class:`Widget` class is the base class
+# required for creating Widgets.
+from kivy.uix.widget import Widget
+
+# The Label widget is for rendering text.
+from kivy.uix.label import Label
+
+# To use the checkbox must import it from this module
+from kivy.uix.checkbox import CheckBox
+
+# The GridLayout arranges children in a matrix.
+from kivy.uix.gridlayout import GridLayout
+
+
+# Container class for the app's widgets
+class check_box(GridLayout):
+    def __init__(self, **kwargs):
+        # super function can be used to gain access
+        # to inherited methods from a parent or sibling class
+        # that has been overwritten in a class object.
+
+        super(check_box, self).__init__(**kwargs)
+
+        # 2 columns in grid layout
+        self.cols = 2
+
+        #Add checkbox, widget & labels
+        self.add_widget(Label(text = 'Male'))
+        self.active = CheckBox(active = True)
+        self.add_widget(self.active)
+
+        self.add_widget(Label(text = 'Female'))
+        self.active = CheckBox(active = True)
+        self.add_widget(self.active)
+
+
+
